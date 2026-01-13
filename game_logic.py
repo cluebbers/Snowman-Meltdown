86975af6a88aaa92e4767cbe6f5ba3da5b605cea
Christopher L. Lübbers <wsad@posteo.net>
2026-01-13 10:17:55 +0100

    pep8 formatting

--- game_logic.py
+++ game_logic.py
@@ -1,5 +1,5 @@
 import random
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
 from ascii_art import STAGES
 
@@ -39,7 +39,11 @@ def play_game():
         # Prompt user for one guess (logic to be enhanced later)
         guess = input(Fore.YELLOW + "Guess a letter: " + Style.RESET_ALL).lower()
         if len(guess) != 1 or not guess.isalpha():
-            print(Fore.RED + "Please enter a single alphabetical character." + Style.RESET_ALL)
+            print(
+                Fore.RED
+                + "Please enter a single alphabetical character."
+                + Style.RESET_ALL
+            )
             continue
         if guess in secret_word:
             guessed_letters.add(guess)
@@ -49,4 +53,3 @@ def play_game():
         print(Fore.GREEN + "Congratulations! You saved the snowman!" + Style.RESET_ALL)
     else:
         print(Fore.RED + "Game Over! The word was:" + secret_word + Style.RESET_ALL)
-    
--- snowman.py
+++ snowman.py
@@ -1,7 +1,8 @@
 import game_logic
+
 if __name__ == "__main__":
     game_logic.play_game()
     play_again = input("Do you want to play again? (y/n): ").lower()
-    while play_again == 'y':
+    while play_again == "y":
         game_logic.play_game()
         play_again = input("Do you want to play again? (y/n): ").lower()
